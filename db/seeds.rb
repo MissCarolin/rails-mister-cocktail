434888c31199b9387087482c7473a715cae077d5
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-02-21 12:46:37 +0100

    adds root path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying cocktais and ingredients"
+Cocktail.destroy_all
+Ingredient.destroy_all
+puts "successfully destroyed"
+
+Ingredient.create(name: "ice")
+Ingredient.create(name: "Chocolate liqueur")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "Espresso")
+Ingredient.create(name: "Peach nectar")
+Ingredient.create(name: "Ginger")
+puts "ingredients created"
+
+Cocktail.create(name: "Basil Smash")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Honey Pot")
+Cocktail.create(name: "Pina Colada")
+puts "cocktails created"
