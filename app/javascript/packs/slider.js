c05a7f2399c32c32cc6af0af3f5d2740f19ee107
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-05-14 19:08:40 +0200

    update

--- app/javascript/packs/slider.js
+++ app/javascript/packs/slider.js
@@ -1,27 +1,23 @@
 const slider = () => {
   const headline = document.querySelector('.headline_cocktail')
-  window.addEventListener('mouseover', (event)  => {
+  window.addEventListener('load', (event)  => {
   headline.classList.add('active');
   });
 
 
   const doses = [...document.querySelectorAll('[data-dose-id]')]
-  window.addEventListener('mouseover', (event) => {
+  window.addEventListener('load', (event) => {
     doses.map(dose => {
       // const targetId = dose.dataset.doseId
       // const targetDose = document.getElementById(`dose-${targetId}`)
       dose.classList.add('activenew')
       console.log(dose)
     })
-
-
-
-
     });
-}
   // window.addEventListener('onpagehide', (event)  => {
   //   headline.classList.remove('active');
   // });
+}
 
 
 export { slider };
