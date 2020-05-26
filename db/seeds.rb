a2a7d12f9a0184cc0e950a63e09b127c66674016
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-05-26 14:53:45 +0200

    delete cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "destroying cocktais and ingredients"
-Cocktail.destroy_all
+# Cocktail.destroy_all
 Ingredient.destroy_all
 puts "successfully destroyed"
 
