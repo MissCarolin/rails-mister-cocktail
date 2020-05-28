4895ceb9e1740766ebd4bf0d29ce8ddd42f4217e
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-05-28 15:27:11 +0200

    layout banner

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (window.location.pathname == '/cocktails' || '/') {
   new Typed('#banner-typed-text', {
-    strings: ["MIX YOUR OWN, BABY"],
+    strings: ["CREATE YOUR OWN, BABY"],
     typeSpeed: 100,
     loop: true
   });
