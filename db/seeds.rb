b03b77f9cb5b78f9cd9dfe9cd0f7dc655043d12f
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-05-31 11:49:18 +0200

    added ingredients to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,36 +10,36 @@ Cocktail.destroy_all
 Ingredient.destroy_all
 puts "successfully destroyed"
 
-Ingredient.create(name: "ice")
-Ingredient.create(name: "chocolate liqueur")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "espresso")
-Ingredient.create(name: "peach nectar")
-Ingredient.create(name: "ginger")
-Ingredient.create(name: "basil leaves")
-Ingredient.create(name: "lemon Juice")
-Ingredient.create(name: "sugar syrup")
-Ingredient.create(name: "crushed ice")
-Ingredient.create(name: "pineapple nectar")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "white rum")
-Ingredient.create(name: "mango mousse")
-Ingredient.create(name: "sparkling water")
-Ingredient.create(name: "lime juice")
-Ingredient.create(name: "cranberry juice")
-Ingredient.create(name: "triple sec")
-Ingredient.create(name: "lemon vodka")
-Ingredient.create(name: "wedges of lemon")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "bourbon")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "campari")
-Ingredient.create(name: "vermouth rosso")
-Ingredient.create(name: "pineapple juice")
-Ingredient.create(name: "coconut cream")
-Ingredient.create(name: "dry vermouth")
-Ingredient.create(name: "olives")
-Ingredient.create(name: "orange juice")
+ingredient1 = Ingredient.create(name: "ice")
+ingredient2 = Ingredient.create(name: "chocolate liqueur")
+ingredient3 = Ingredient.create(name: "gin")
+ingredient4 = Ingredient.create(name: "espresso")
+ingredient5 = Ingredient.create(name: "peach nectar")
+ingredient6 = Ingredient.create(name: "ginger")
+ingredient7 = Ingredient.create(name: "basil leaves")
+ingredient8 = Ingredient.create(name: "lemon Juice")
+ingredient9 = Ingredient.create(name: "sugar syrup")
+ingredient10 = Ingredient.create(name: "crushed ice")
+ingredient11 = Ingredient.create(name: "pineapple nectar")
+ingredient12 = Ingredient.create(name: "vodka")
+ingredient13 = Ingredient.create(name: "white rum")
+ingredient14 = Ingredient.create(name: "mango mousse")
+ingredient15 = Ingredient.create(name: "sparkling water")
+ingredient16 = Ingredient.create(name: "lime juice")
+ingredient17 = Ingredient.create(name: "cranberry juice")
+ingredient18 = Ingredient.create(name: "triple sec")
+ingredient19 = Ingredient.create(name: "lemon vodka")
+ingredient20 = Ingredient.create(name: "wedges of lemon")
+ingredient21 = Ingredient.create(name: "mint leaves")
+ingredient22 = Ingredient.create(name: "bourbon")
+ingredient23 = Ingredient.create(name: "gin")
+ingredient24 = Ingredient.create(name: "campari")
+ingredient25 = Ingredient.create(name: "vermouth rosso")
+ingredient26 = Ingredient.create(name: "pineapple juice")
+ingredient27 = Ingredient.create(name: "coconut cream")
+ingredient28 = Ingredient.create(name: "dry vermouth")
+ingredient29 = Ingredient.create(name: "olives")
+ingredient30 = Ingredient.create(name: "orange juice")
 puts "ingredients created"
 
 cocktail1 = Cocktail.create(name: "Basil Smash")
@@ -78,5 +78,15 @@ cocktail9 = Cocktail.create(name: "Classic Martini")
 file = URI.open('https://res.cloudinary.com/misscarolin/image/upload/v1590825633/martini_kLEIN_kwax1x.jpg')
 cocktail9.photo.attach(io: file, filename: 'Martini.jpeg', content_type: 'image/jpeg')
 
-
 puts "cocktails created"
+
+dose = Dose.create(cocktail_id: cocktail1.id, ingredient_id: ingredient7.id, description: 'a bunch of');
+dose1 = Dose.create(cocktail_id: cocktail1.id, ingredient_id: ingredient3.id, description: '50 ml');
+dose3 = Dose.create(cocktail_id: cocktail1.id, ingredient_id: ingredient8.id, description: '25 ml');
+dose5 = Dose.create(cocktail_id: cocktail1.id, ingredient_id: ingredient9.id, description: '15 ml');
+dose4 = Dose.create(cocktail_id: cocktail1.id, ingredient_id: ingredient1.id, description: '2-3 cubes of');
+
+
+
+
+puts "doses created"
