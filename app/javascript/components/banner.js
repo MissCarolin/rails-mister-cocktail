979dd78b250810571b944698024854e1a6d39c34
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-06-22 23:25:28 +0200

    fixed js

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  if (window.location.pathname == '/cocktails' || '') {
+  if (window.location.pathname == '/cocktails' || '/') {
   new Typed('#banner-typed-text', {
     strings: ["CREATE YOUR OWN, BABY"],
     typeSpeed: 100,
